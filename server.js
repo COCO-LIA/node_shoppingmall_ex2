da5c7432690fd8fe5a81652c2169f4f7c7a5f64c
LIA <lvxing29@naver.com>
2020-11-21 14:00:40 +0900

    my second node server

---
+++ server.js
@@ -0,0 +1,13 @@
+
+//코드 빌드 순서
+//첫번째: =를 기준으로 오른쪽에서 왼쪽으로 대입한다.
+//두번째: 코드 빌드 순서는 위에서 아래로
+//세번째: 하위 메소드를 불러오는 방법: .으로 표시한다.
+
+const express = require('express')
+
+const app = express()
+
+const port = 5001
+
+app.listen(port, console.log("Server started"))
