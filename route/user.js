b4536cebc88e61fb0cbcd2c04a613e89701017b6
LIA <lvxing29@naver.com>
2020-12-02 23:00:51 +0900

    회원가입 #2 (join member2)

--- route/user.js
+++ route/user.js
@@ -2,34 +2,47 @@
 
 const express = require("express")
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 const userModel = require('../model/user')
 
 //회원가입 API
 router.post("/register", (req, res) => {
 
-    const userInfo = new userModel({
-        username: req.body.unm,
-        email:req.body.em,
-        password:req.body.pw
+    bcrypt.hash(req.body.pw, 10, (err, hash) => {
 
+        if(err){
+            return res.json({
+                error: err
+            })
+        } else {
+            const userInfo = new userModel({
+                username: req.body.unm,
+                email:req.body.em,
+                password:hash
+
+            })
+
+            userInfo
+                .save()
+                .then(user => {
+                    res.json({
+                        msg: "회원가입",
+                        userInfo: user
+
+
+                    })
+                })
+                .catch(err => {
+                    res.json({
+                        msg: err.message
+                    })
+                })
+        }
     })
 
-    userInfo
-        .save()
-        .then(user => {
-            res.json({
-                msg: "회원가입",
-                userInfo: user
 
 
-            })
-        })
-        .catch(err => {
-            res.json({
-                msg: err.message
-            })
-        })
 })
 
 
